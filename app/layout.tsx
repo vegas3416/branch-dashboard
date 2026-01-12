da27ba92d5ce178e83cb1ca922f9887aa06e67a8
Jason <jason@Jasons-MacBook-Pro.local>
2026-01-11 18:13:16 -0600

    small tab naming update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,6 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Providers from './providers';
-import { Raleway } from 'next/font/google';
-
-const raleway = Raleway({
-  subsets: ['latin'],
-  variable: '--font-sans',
-  display: 'swap',
-});
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
